3334d2dfe0077b6bb8c2ea16db0b09d7c5f1d15c
alparikan <159568834+alparikan@users.noreply.github.com>
2024-02-19 22:41:14 +0300

    Add files via upload

---
+++ ArtikYilHesaplama.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArtikYilHesaplama {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        year = scan.nextInt();
+        System.out.println();
+
+        if((year % 4) == 0) {
+            if((year % 100) == 0) {
+                if((year % 400) == 0) {
+                    System.out.println(year + " bir artık yıldır !");
+                }
+                else {
+                    System.out.println(year + " bir artık yıl değildir !");
+                }
+            }
+            else {
+                System.out.println(year + " bir artık yıldır !");
+            }
+        }
+        else {
+            System.out.println(year + " bir artık yıl değildir !");
+        }
+
+    }
+}
