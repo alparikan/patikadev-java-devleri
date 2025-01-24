293dd0a73965e8890e3b718efbd569838a6e97dc
alparikan <159568834+alparikan@users.noreply.github.com>
2025-01-24 21:45:35 +0300

    Add files via upload

---
+++ PalindromSayılar.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromSayılar {
+
+    public static void palindromMu(int num) {
+        int gecici = num, tersi = 0, kalan = 0;
+
+        while(gecici != 0) {
+           kalan = gecici % 10;
+           tersi = (tersi * 10) + kalan;
+           gecici /= 10;
+        }
+        if(num == tersi) {
+            System.out.println(num + " is a palindrom number.");
+        }
+        else {
+            System.out.println(num + " is not a palindrom number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number please: ");
+        int a = scan.nextInt();
+        palindromMu(a);
+    }
+}
